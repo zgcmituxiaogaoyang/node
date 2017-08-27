24cdba1ddf89cc11f24f81c77c55e87a02f73c08
zgcmituxiaogaoyang <13540641425@163.com>
2017-08-27 18:27:06 +0800

    node os module

---
+++ os.js
@@ -0,0 +1,11 @@
+os=require('os');
+// tot=os.totalmem();
+// tot=os.freemem();
+// tot=os.tmpdir();
+// tot=os.hostname();
+// tot=os.type();
+// tot=os.platform();
+// tot=os.loadavg();
+// tot=os.cpus();
+tot=os.networkInterfaces();
+console.log(tot);
